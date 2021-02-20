7178f0fe3d5e18c458b2ae60e26d890b044f76b5
SAURAV KUMAR SONU <57924568+ItsmeSauravSonu@users.noreply.github.com>
2021-02-21 01:52:11 +0530

    Books Create Index New

--- routes/books.js
+++ routes/books.js
@@ -19,8 +19,15 @@ const upload = multer({
 router.get('/',async (req,res)=>{
     let query = Book.find()
     if(req.query.title!=null && req.query.title!=''){
-        query=query.regex('tite', new RegExp(req.query.title,'i'))
+        query=query.regex('title', new RegExp(req.query.title,'i'))
     }
+    if(req.query.publishedBefore!=null && req.query.publishedBefore!=''){
+        query=query.lte('publishDate', req.query.publishedBefore)
+    }
+    if(req.query.publishedAfter!=null && req.query.publishedAfter!=''){
+        query=query.gte('publishDate', req.query.publishedAfter)
+    }
+    
     try{
         const books = await query.exec()
         res.render('books/index',{
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const router = express.Router()
-
-router.get('/',(req,res)=>{
-    res.render('index')
+const Book = require('../models/book')
+router.get('/',async (req,res)=>{
+    let books
+    try{
+books = await Book.find().sort({createdAt:'desc'}).limit(10).exec()
+    }catch{
+books=[]
+    }
+    res.render('index',{books:books})
 })
 
 module.exports = router
